dbb31a983322ba67ab330d34ac27eda297d4829c
Erik Aksland <erikaksland15@gmail.com>
2020-12-03 22:13:22 -0500

    use axios to get the user info in the Users Page

---
+++ src/components/Navigation.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav id="nav-links">
+    <router-link to="/" class="nav-link">Home</router-link>
+    <router-link to="/users/" class="nav-link">User Table</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navigation'
+}
+</script>
+
+<style scoped>
+  nav .nav-link {
+    padding: 12px 16px;
+    line-height: 16px;
+  }
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="users">
+    <h2>My Users</h2>
+    <div v-for="user in users" v-bind:key="user.key" class="currency">
+      {{ user.name }}:
+      <span >
+        {{ user.username | user.email }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+const axios = require('axios').default;
+const url = "https://jsonplaceholder.typicode.com/users";
+
+export default {
+  name: 'Users',
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    axios.get(url).then(response => {
+      this.users = response.data
+      console.log(this.users);
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
